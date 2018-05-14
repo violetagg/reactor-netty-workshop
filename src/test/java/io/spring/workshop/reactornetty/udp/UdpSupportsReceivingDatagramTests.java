18bf73c1b3014c5f484a9dd9a1b9b319552a6378
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-14 19:33:54 +0300

    [UdpSupportsReceivingDatagramTests] Add UDP server IO handler that will process the incoming packages

--- src/test/java/io/spring/workshop/reactornetty/udp/UdpSupportsReceivingDatagramTests.java
+++ src/test/java/io/spring/workshop/reactornetty/udp/UdpSupportsReceivingDatagramTests.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 import reactor.netty.Connection;
 import reactor.netty.resources.LoopResources;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 /**
  * Learn how to create UDP server
@@ -15,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class UdpSupportsReceivingDatagramTests {
 
     @Test
-    public void supportsReceivingDatagramTest() {
+    public void supportsReceivingDatagramTest() throws Exception {
         // TODO
         // Task 1:
         // 1.1. Prepare the UDP server
@@ -25,7 +30,14 @@ public class UdpSupportsReceivingDatagramTests {
         // 1.5. Create a new simple LoopResources
         // 1.6. Configure the UDP server to run on this newly created LoopResources
         // 1.7. Apply a wire logger configuration
+        //
+        // Task 6:
+        // 6.1. Attach an IO handler
+        // 6.2. When receive a package, transform it to byte array
+        // 6.3. On every emitted byte array, inspect the byte array's length
+        //      and if it is the expected size, decrement the count of the latch
         LoopResources loopResources = null;
+        CountDownLatch latch = new CountDownLatch(4);
         Connection server1 = null;
 
         assertNotNull(server1);
@@ -46,6 +58,8 @@ public class UdpSupportsReceivingDatagramTests {
         Connection server2 = null;
 
         assertNotNull(server2);
+
+        assertTrue(latch.await(30, TimeUnit.SECONDS));
 
         // TODO
         // Task 2:
