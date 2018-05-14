39fbacfd8a741574346f2b5782fda549de472fef
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-14 15:42:50 +0300

    [UdpEchoTests] Add UDP server IO handler that will echo the received message

--- src/test/java/io/spring/workshop/reactornetty/udp/UdpEchoTests.java
+++ src/test/java/io/spring/workshop/reactornetty/udp/UdpEchoTests.java
@@ -1,6 +1,11 @@
 package io.spring.workshop.reactornetty.udp;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.util.CharsetUtil;
 import org.junit.Test;
+import reactor.core.publisher.Mono;
 import reactor.netty.Connection;
 import reactor.netty.resources.LoopResources;
 import reactor.netty.udp.UdpServer;
@@ -25,6 +30,26 @@ public class UdpEchoTests {
                                    // an ephemeral port when binding the server.
                          .runOn(loopResources) // Configures the UDP server to run on a specific LoopResources.
                          .wiretap()            // Applies a wire logger configuration.
+                         .handle((in, out) ->
+                                 in.receiveObject() // Obtains the inbound Flux.
+                                   .map(o -> { // Transforms the datagram.
+                                       if (o instanceof DatagramPacket) {
+                                           // Incoming DatagramPacket
+                                           DatagramPacket p = (DatagramPacket) o;
+                                           ByteBuf buf1 = Unpooled.copiedBuffer("Hello ", CharsetUtil.UTF_8);
+                                           // Creates a new ByteBuf using the incoming DatagramPacket content.
+                                           ByteBuf buf2 = Unpooled.copiedBuffer(buf1, p.content()
+                                                                                       .retain());
+                                           // Creates a new DatagramPacket with the ByteBuf and the sender
+                                           // information from the incoming DatagramPacket.
+                                           return new DatagramPacket(buf2, p.sender());
+                                       }
+                                       else {
+                                           return Mono.error(new Exception("Unexpected type of the message: " + o));
+                                       }
+                                   })
+                                   .flatMap(out::sendObject)
+                                   .log("udp-server"))
                          .bind()   // Binds the UDP server and returns a Mono<Connection>.
                          .block(); // Blocks and waits the server to finish initialising.
 
