e1838fc716aa15a0fe478a50af9a2f9137a8a0bd
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-15 13:02:15 +0300

    [TcpSendFileTests]  Add TCP server IO handler
    The TCP server will receive a file name as incoming message
    and will return the file with that name as response.

--- src/test/java/io/spring/workshop/reactornetty/tcp/TcpSendFileTests.java
+++ src/test/java/io/spring/workshop/reactornetty/tcp/TcpSendFileTests.java
@@ -4,8 +4,13 @@ import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.junit.Test;
+import reactor.core.publisher.Mono;
 import reactor.netty.DisposableServer;
 import reactor.netty.tcp.TcpServer;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -29,6 +34,19 @@ public class TcpSendFileTests {
                                      // an ephemeral port when binding the server.
                          .secure(spec -> spec.sslContext(sslContextBuilder))   // Use self singed certificate.
                          .wiretap()  // Applies a wire logger configuration.
+                         .handle((in, out) ->
+                                 in.receive()
+                                   .asString()
+                                   .flatMap(s -> {
+                                       try {
+                                           Path file = Paths.get(getClass().getResource(s).toURI());
+                                           return out.sendFile(file)
+                                                     .then();
+                                       } catch (URISyntaxException e) {
+                                           return Mono.error(e);
+                                       }
+                                   })
+                                   .log("tcp-server"))
                          .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish initializing.
 
         assertNotNull(server);
