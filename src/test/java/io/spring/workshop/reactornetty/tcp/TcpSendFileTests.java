4a5aab904a3fac91605848453f13859b41f221a0
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-15 12:21:43 +0300

    [TcpSendFileTests] Configure TCP Server

--- src/test/java/io/spring/workshop/reactornetty/tcp/TcpSendFileTests.java
+++ src/test/java/io/spring/workshop/reactornetty/tcp/TcpSendFileTests.java
@@ -1,5 +1,8 @@
 package io.spring.workshop.reactornetty.tcp;
 
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.junit.Test;
 import reactor.netty.DisposableServer;
 import reactor.netty.tcp.TcpServer;
@@ -16,11 +19,16 @@ import static org.junit.Assert.assertNotNull;
 public class TcpSendFileTests {
 
     @Test
-    public void sendFileTest() {
+    public void sendFileTest() throws Exception {
+        SelfSignedCertificate cert = new SelfSignedCertificate();
+        SslContextBuilder sslContextBuilder =
+               SslContextBuilder.forServer(cert.certificate(), cert.privateKey());
         DisposableServer server =
                 TcpServer.create()   // Prepares a TCP server for configuration.
                          .port(0)    // Configures the port number as zero, this will let the system pick up
                                      // an ephemeral port when binding the server.
+                         .secure(spec -> spec.sslContext(sslContextBuilder))   // Use self singed certificate.
+                         .wiretap()  // Applies a wire logger configuration.
                          .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish initializing.
 
         assertNotNull(server);
