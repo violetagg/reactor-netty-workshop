164d402806001d5476840548320641e770856773
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-15 16:19:57 +0300

    [HttpCompressionTests] Create HTTP server and add IO handler that will return compressed response

---
+++ src/test/java/io/spring/workshop/reactornetty/http/HttpCompressionTests.java
@@ -0,0 +1,34 @@
+package io.spring.workshop.reactornetty.http;
+
+import org.junit.Test;
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Learn how to create HTTP server and client
+ *
+ * @author Violeta Georgieva
+ * @see <a href="http://next.projectreactor.io/docs/netty/snapshot/api/reactor/netty/http/server/HttpServer.html">HttpServer javadoc</a>
+ * @see <a href="http://next.projectreactor.io/docs/netty/snapshot/api/reactor/netty/http/client/HttpClient.html">HttpClient javadoc</a>
+ */
+public class HttpCompressionTests {
+
+    @Test
+    public void httpCompressionTest() {
+        DisposableServer server =
+                HttpServer.create()   // Prepares a HTTP server for configuration.
+                          .port(0)    // Configures the port number as zero, this will let the system pick up
+                                      // an ephemeral port when binding the server.
+                          .handle((req, res) -> res.sendString(Mono.just("compressed response")))
+                          .compress(true) // Enables compression.
+                          .wiretap()  // Applies a wire logger configuration.
+                          .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish initializing.
+
+        assertNotNull(server);
+
+        server.disposeNow();          // Stops the server and releases the resources.
+    }
+}
