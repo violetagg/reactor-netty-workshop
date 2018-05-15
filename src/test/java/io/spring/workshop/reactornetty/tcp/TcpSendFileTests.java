cd128d69c5f7094abf13e415d9a132e53b8e0e5b
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-15 12:19:41 +0300

    [TcpSendFileTests] Create TCP server

---
+++ src/test/java/io/spring/workshop/reactornetty/tcp/TcpSendFileTests.java
@@ -0,0 +1,30 @@
+package io.spring.workshop.reactornetty.tcp;
+
+import org.junit.jupiter.api.Test;
+import reactor.netty.DisposableServer;
+import reactor.netty.tcp.TcpServer;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Learn how to create TCP server and client
+ *
+ * @author Violeta Georgieva
+ * @see <a href="https://projectreactor.io/docs/netty/release/api/reactor/netty/tcp/TcpServer.html">TcpServer javadoc</a>
+ * @see <a href="https://projectreactor.io/docs/netty/release/api/reactor/netty/tcp/TcpClient.html">TcpClient javadoc</a>
+ */
+public class TcpSendFileTests {
+
+    @Test
+    public void sendFileTest() {
+        DisposableServer server =
+                TcpServer.create()   // Prepares a TCP server for configuration.
+                         .port(0)    // Configures the port number as zero, this will let the system pick up
+                                     // an ephemeral port when binding the server.
+                         .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish initializing.
+
+        assertNotNull(server);
+
+        server.disposeNow(); // Stops the server and releases the resources.
+    }
+}
