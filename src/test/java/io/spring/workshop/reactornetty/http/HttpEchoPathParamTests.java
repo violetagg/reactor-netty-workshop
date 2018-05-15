f8c0d6239735f7e4ebde4024ce49c69b082ec513
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-15 15:13:15 +0300

    [HttpEchoPathParamTests] Create HTTP server

---
+++ src/test/java/io/spring/workshop/reactornetty/http/HttpEchoPathParamTests.java
@@ -0,0 +1,30 @@
+package io.spring.workshop.reactornetty.http;
+
+import org.junit.jupiter.api.Test;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Learn how to create HTTP server and client
+ *
+ * @author Violeta Georgieva
+ * @see <a href="https://projectreactor.io/docs/netty/release/api/reactor/netty/http/server/HttpServer.html">HttpServer javadoc</a>
+ * @see <a href="https://projectreactor.io/docs/netty/release/api/reactor/netty/http/client/HttpClient.html">HttpClient javadoc</a>
+ */
+public class HttpEchoPathParamTests {
+
+    @Test
+    public void echoPathParamTest() {
+        DisposableServer server =
+                HttpServer.create()   // Prepares a TCP server for configuration.
+                          .port(0)    // Configures the port number as zero, this will let the system pick up
+                                      // an ephemeral port when binding the server.
+                          .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish initializing.
+
+        assertNotNull(server);
+
+        server.disposeNow();          // Stops the server and releases the resources.
+    }
+}
