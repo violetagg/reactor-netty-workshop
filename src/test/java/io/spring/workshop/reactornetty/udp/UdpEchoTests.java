1b41363cf01fa916fe0a5b72cceb3381e0f4c4b5
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-15 10:16:02 +0300

    [UdpEchoTests] Create UDP server

---
+++ src/test/java/io/spring/workshop/reactornetty/udp/UdpEchoTests.java
@@ -0,0 +1,31 @@
+package io.spring.workshop.reactornetty.udp;
+
+import org.junit.Test;
+import reactor.netty.Connection;
+import reactor.netty.udp.UdpServer;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Learn how to create a UDP server and client
+ *
+ * @author Violeta Georgieva
+ * @see <a href="http://next.projectreactor.io/docs/netty/snapshot/api/reactor/netty/udp/UdpServer.html">UdpServer javadoc</a>
+ * @see <a href="http://next.projectreactor.io/docs/netty/snapshot/api/reactor/netty/udp/UdpClient.html">UdpClient javadoc</a>
+ */
+public class UdpEchoTests {
+
+    @Test
+    public void echoTest() {
+        Connection server =
+                UdpServer.create() // Prepares a UDP server for configuration.
+                         .port(0)  // Configures the port number as zero, this will let the system pick up
+                                   // an ephemeral port when binding the server.
+                         .bind()   // Binds the UDP server and returns a Mono<Connection>.
+                         .block(); // Blocks and waits the server to finish initialising.
+
+        assertNotNull(server);
+
+        server.disposeNow();       // Stops the server and releases the resources.
+    }
+}
