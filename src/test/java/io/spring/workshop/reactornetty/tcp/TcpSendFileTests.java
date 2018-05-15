ae3ecae2cbba33a4e225b5e1c8cf3bdeb8c94b82
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-15 12:21:43 +0300

    [TcpSendFileTests] Configure TCP Server

--- src/test/java/io/spring/workshop/reactornetty/tcp/TcpSendFileTests.java
+++ src/test/java/io/spring/workshop/reactornetty/tcp/TcpSendFileTests.java
@@ -1,8 +1,10 @@
 package io.spring.workshop.reactornetty.tcp;
 
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.junit.jupiter.api.Test;
 import reactor.netty.DisposableServer;
 import reactor.netty.tcp.TcpServer;
+import reactor.netty.tcp.TcpSslContextSpec;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -16,11 +18,16 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class TcpSendFileTests {
 
     @Test
-    public void sendFileTest() {
+    public void sendFileTest() throws Exception {
+        SelfSignedCertificate cert = new SelfSignedCertificate();
+        TcpSslContextSpec sslContextBuilder =
+                TcpSslContextSpec.forServer(cert.certificate(), cert.privateKey());
         DisposableServer server =
                 TcpServer.create()   // Prepares a TCP server for configuration.
                          .port(0)    // Configures the port number as zero, this will let the system pick up
                                      // an ephemeral port when binding the server.
+                         .secure(spec -> spec.sslContext(sslContextBuilder))   // Use self singed certificate.
+                         .wiretap(true)  // Applies a wire logger configuration.
                          .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish initializing.
 
         assertNotNull(server);
