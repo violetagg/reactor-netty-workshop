5d3aca03367db2e2f19793cb60fcf15aa46ce161
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-22 19:46:34 +0300

    [HttpEncodeAndDecodeJsonTests] Create HTTP server and add IO handler that will handle JSON
    Add additional handler to the pipeline.
    Use JSON decoder for the request and JSON encoder for the response

--- src/test/java/io/spring/workshop/reactornetty/http/HttpEncodeAndDecodeJsonTests.java
+++ src/test/java/io/spring/workshop/reactornetty/http/HttpEncodeAndDecodeJsonTests.java
@@ -3,11 +3,18 @@ package io.spring.workshop.reactornetty.http;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.json.JsonObjectDecoder;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
 
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Learn how to create HTTP server and client
@@ -41,6 +48,30 @@ public class HttpEncodeAndDecodeJsonTests {
                 throw new RuntimeException(e);
             }
         };
+    }
+
+    @Test
+    public void encodeAndDecodeJsonTest() {
+        DisposableServer server =
+                HttpServer.create()   // Prepares a HTTP server for configuration.
+                          .port(0)    // Configures the port number as zero, this will let the system pick up
+                                      // an ephemeral port when binding the server.
+                          .handle((in, out) ->
+                                  // Calls the passed callback with Connection to operate on the
+                                  // underlying channel state. This allows changing the channel pipeline.
+                                  out.send(in.withConnection(c -> c.addHandler(new JsonObjectDecoder()))
+                                             .receive()
+                                             .asString()
+                                             .map(jsonDecoder)
+                                             .concatMap(Flux::fromArray)
+                                             .window(5)
+                                             .concatMap(w -> w.collectList().map(jsonEncoder))))
+                          .wiretap(true)  // Applies a wire logger configuration.
+                          .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish initializing.
+
+        assertNotNull(server);
+
+        server.disposeNow();          // Stops the server and releases the resources.
     }
 
     private static final class Pojo {
