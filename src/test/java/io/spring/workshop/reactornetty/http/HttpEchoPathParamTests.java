7a8f2e2ce9355af4e55b98311eaad477e48575c6
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-22 19:10:24 +0300

    [HttpEchoPathParamTests] Define request/response for HTTP Client
    The client will add a header, then will prepare a POST request and will
    send a message to the server. The received response will be aggregated
    and transformed to string.

--- src/test/java/io/spring/workshop/reactornetty/http/HttpEchoPathParamTests.java
+++ src/test/java/io/spring/workshop/reactornetty/http/HttpEchoPathParamTests.java
@@ -1,9 +1,13 @@
 package io.spring.workshop.reactornetty.http;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.netty.ByteBufMono;
 import reactor.netty.DisposableServer;
+import reactor.netty.http.client.HttpClient;
 import reactor.netty.http.server.HttpServer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
@@ -33,6 +37,23 @@ public class HttpEchoPathParamTests {
                           .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish initializing.
 
         assertNotNull(server);
+
+        String response =
+                HttpClient.create()             // Prepares an HTTP client for configuration.
+                          .port(server.port())  // Obtains the server's port and provides it as a port to which this
+                                                // client should connect.
+                          .wiretap(true)        // Applies a wire logger configuration.
+                          .headers(h -> h.add("Content-Type", "text/plain")) // Adds headers to the HTTP request
+                          .post()              // Specifies that POST method will be used
+                          .uri("/test/World")  // Specifies the path
+                          .send(ByteBufMono.fromString(Mono.just("Hello")))  // Sends the request body
+                          .responseContent()   // Receives the response body
+                          .aggregate()
+                          .asString()
+                          .log("http-client")
+                          .block();
+
+        assertEquals("Hello World!", response);
 
         server.disposeNow();          // Stops the server and releases the resources.
     }
