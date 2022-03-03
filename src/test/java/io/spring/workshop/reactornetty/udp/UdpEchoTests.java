635de6f38f216a53cfa4b4097609953b8761eb1e
Violeta Georgieva <violetag@vmware.com>
2022-03-03 11:20:46 +0200

    [UdpEchoTests] Configure UDP Server

--- src/test/java/io/spring/workshop/reactornetty/udp/UdpEchoTests.java
+++ src/test/java/io/spring/workshop/reactornetty/udp/UdpEchoTests.java
@@ -2,6 +2,7 @@ package io.spring.workshop.reactornetty.udp;
 
 import org.junit.jupiter.api.Test;
 import reactor.netty.Connection;
+import reactor.netty.resources.LoopResources;
 import reactor.netty.udp.UdpServer;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -17,10 +18,13 @@ public class UdpEchoTests {
 
     @Test
     public void echoTest() {
+        LoopResources loopResources = LoopResources.create("echo-test-udp");
         Connection server =
                 UdpServer.create() // Prepares a UDP server for configuration.
                          .port(0)  // Configures the port number as zero, this will let the system pick up
                                    // an ephemeral port when binding the server.
+                         .runOn(loopResources) // Configures the UDP server to run on a specific LoopResources.
+                         .wiretap(true)        // Applies a wire logger configuration.
                          .bind()   // Binds the UDP server and returns a Mono<Connection>.
                          .block(); // Blocks and waits the server to finish initialising.
 
